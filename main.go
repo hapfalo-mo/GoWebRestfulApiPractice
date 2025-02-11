e41e3355fadd38045e49d095d37063400c0ce01e
NQHChuong <nguyenquochuychuong16112002@gmail.com>
2025-02-11 17:38:21 +0700

    Chuong: Add API get Animal

--- main.go
+++ main.go
@@ -21,15 +21,31 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+type animal struct {
+	ID    int    `json:"aid"`
+	Name  string `json:"aname"`
+	Color string `json:"acolor"`
+}
+
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
+var animals = []animal{
+	{ID: 1, Name: "Dog", Color: "Black"},
+	{ID: 2, Name: "Cat", Color: "White"},
+	{ID: 3, Name: "Bird", Color: "Yellow"},
+}
 
 // get full list of albums
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// get full list of animals
+func getAnimals(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, animals)
 }
 func main() {
 	r := gin.Default()
@@ -41,5 +57,6 @@ func main() {
 	// ReturnListOfAnima(r)
 	// Get full list of albums
 	r.GET("/getAlbums", getAlbums)
+	r.GET("/getAnimals", getAnimals)
 	r.Run("localhost:8080")
 }
