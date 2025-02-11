4145fabd02b62831a4bcf9edb5712db40065a111
NQHChuong <nguyenquochuychuong16112002@gmail.com>
2025-02-11 15:00:00 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ReturnListOfAnima(r *gin.Engine) {
+	r.GET("/animals", func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": "Not found animal",
+		})
+	})
+}
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// get full list of albums
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+func main() {
+	r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// ReturnListOfAnima(r)
+	// Get full list of albums
+	r.GET("/getAlbums", getAlbums)
+	r.Run("localhost:8080")
+}
---
+++ models/Book.go
@@ -0,0 +1,8 @@
+
+// import "github.com/jinzhu/gorm"
+
+// type Book struct {
+// 	ID string, `json:"id"`
+// 	Title string, `jsonL"title"`
+// 	Author string, `json:"author"`
+// }
