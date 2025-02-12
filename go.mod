d08d66e04ce235f4c2891ab99e230977cd960dfc
NQHChuong <nguyenquochuychuong16112002@gmail.com>
2025-02-12 10:28:25 +0700

    Connect Go wwith db SQL Server Mangement Studio 2020

--- go.mod
+++ go.mod
@@ -9,12 +9,15 @@ require (
 	github.com/bytedance/sonic/loader v0.2.3 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v1.0.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.24.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -25,7 +28,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.14.0 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -27,16 +28,30 @@ type animal struct {
 	Color string `json:"acolor"`
 }
 
+// Struct album
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
+
+// Struct animal
 var animals = []animal{
 	{ID: 1, Name: "Dog", Color: "Black"},
 	{ID: 2, Name: "Cat", Color: "White"},
 	{ID: 3, Name: "Bird", Color: "Yellow"},
 }
+
+// Create Sql Server Configuration (Update credentials accordingly)
+const (
+	server   = "localhost"
+	port     = "1433"
+	user     = "sa"
+	password = "HJ10xugb123*"
+	database = "UserManagement"
+)
+
+var db *sql.DB
 
 // get full list of albums
 func getAlbums(c *gin.Context) {
@@ -47,9 +62,22 @@ func getAlbums(c *gin.Context) {
 func getAnimals(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, animals)
 }
+
+// func add new Animal to the list
+func addAnimal(c *gin.Context) {
+	var newAnimal animal
+	// Call BindJson to bind the recieved Json to newAnimal
+	if err := c.BindJSON(&newAnimal); err != nil {
+		return
+	}
+	// Add the new album to the slice.
+	animals = append(animals, newAnimal)
+	c.IndentedJSON(http.StatusCreated, newAnimal)
+}
 func main() {
 	r := gin.Default()
 	r.GET("/getAlbums", getAlbums)
 	r.GET("/getAnimals", getAnimals)
+	r.POST("/addAnimal", addAnimal)
 	r.Run("localhost:8080")
 }
