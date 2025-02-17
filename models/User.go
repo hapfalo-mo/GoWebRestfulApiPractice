3ba89b8831112402b1690a055486aabf261aac9b
Chuong Nguyen <nguyenquochuychuong16112002@gmail.com>
2025-02-17 22:27:33 +0700

    Create Restful CRUD API

--- models/User.go
+++ models/User.go
@@ -1,8 +1,11 @@
 package models
 
 type User struct {
-	Username string `json:"username"`
-	FullName string `json:"fullname"`
-	RoleId   int8   `json:"roleid"`
-	Password string `json:"password"`
+	UserID          int     `json:"user_id"`
+	Username        string  `json:"username"`
+	UserEmail       string  `json:"user_email"`
+	UserPhoneNumber string  `json:"user_phone_number"`
+	UserCreatedAt   string  `json:"user_created_at"`
+	UserUpdatedAt   string  `json:"user_updated_at"`
+	UserDeletedAt   *string `json:"user_deleted_at"`
 }
